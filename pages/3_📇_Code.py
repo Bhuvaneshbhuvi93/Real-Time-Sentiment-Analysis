70ac947c5fb52204a8e7aab3b3edac5a957e2665
Bhuvaneshwar <bhuvaneshwargomathi@gmail.com>
2023-03-16 11:46:45 +0530

    Dispalying code

--- pages/3_📇_Code.py
+++ pages/3_📇_Code.py
@@ -4,15 +4,38 @@ import base64
 st.set_page_config(
     layout = "wide",
     page_title = "RT Sentiment Analysis",
-    page_icon = "🔥",
+    page_icon = "🔥", 
 )
- 
-st.title("PDF of the Source Code") 
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
 
+st.subheader("Importing Dependensies")
+code = '''from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')'''
 
-show_pdf('Real Time Sentiment Analysis.pdf')
+st.code(code, language='python')
+
+st.subheader("Sentiment Analyser")
+code = '''user_review = input('Please Rate Our Service: ')
+sia = SentimentIntensityAnalyzer()
+score = sia.polarity_scores(user_review)
+print(score)'''
+
+st.write("{'neg': 1.0, 'neu': 0.0, 'pos': 0.0, 'compound': -0.5423}")
+
+st.code(code, language='python')
+
+st.subheader("Final Prediction")
+code = '''user_review = input('Please Rate Our Service: ')
+sia = SentimentIntensityAnalyzer()
+scores = sia.polarity_scores(user_review)
+sentiments_score = [scores['neg'],scores['neu'],scores['pos']]
+if sentiments_score[0] > sentiments_score[2]:
+print('Negative')
+elif sentiments_score[2] > sentiments_score[0]:
+print('Positive')
+else:
+print('Neutral')'''
+
+st.write("Neutral")
+
+st.code(code, language='python')
